dbeaf88daa1c19b9cbab693f2ab1afcb4d8e4d42
Jin An <jin.an@dtcc.edu>
2024-02-06 14:44:06 -0500

    Create Main.java

---
+++ week5/tuesday/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        java.util.Queue<Character> charQueue = new LinkedList<>();
+
+        List<Integer> intList = new LinkedList<>();
+
+        charQueue.add('c');
+
+    }
+}
