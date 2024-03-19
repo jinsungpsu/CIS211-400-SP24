99dbdb078a791f2ad80dddb93e402db46e4e6d87
Jin An <jin.an@dtcc.edu>
2024-03-19 15:11:20 -0400

    Create Main.java

---
+++ week10/tuesday/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMapClosed<>();
+        Map<Integer, String> map = new HashMapOpen<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
+    }
+}
