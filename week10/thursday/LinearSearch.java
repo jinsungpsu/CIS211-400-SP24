573ed5c16b21d453c6c046c7c8fd82f2569e2b33
Jin An <jin.an@dtcc.edu>
2024-03-21 13:53:57 -0400

    Create LinearSearch.java

---
+++ week10/thursday/LinearSearch.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,5,9,3,2,4};
+
+        int i = indexOf(arr, 9);
+        // i should be 2
+        int j = indexOf(arr, 3);
+        // j should be 3
+        int k = indexOf(arr, 99);
+        // k should be -1, because not found
+    }
+
+    static int indexOf(int[] arr, int val) {
+
+        return 0;
+    }
+}
